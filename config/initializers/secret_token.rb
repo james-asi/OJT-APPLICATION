0caa52c81daf467b67ad63dcd4e90ac5824c4667
James Dela Cruz <james@asi.com.ph>
2012-05-28 11:19:01 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OjtApps::Application.config.secret_token = '81420e6b412745a85c610d65abf48afe059e0103897762187b12bc3db72ba8bb53712c64e5f0113abc92790840076e426d50e188cb092160397714040021e48b'
